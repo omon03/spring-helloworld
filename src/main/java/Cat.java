5690a9492d6aa32d9b278c49e841d3a1709852f0
omon03 <omon03@gmail.com>
2021-04-01 19:01:39 +0500

    complete

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String str;
+
+    public void setMessage(String s) {
+        this.str = s;
+    }
+
+    public String getMessage() {
+        return str;
+    }
+}
